72f82e822c6b82837fce1f355a173e1a7e852ae9
tech-shamp <shampytechnical@gmail.com>
2022-08-03 02:12:07 +0500

    Edited kali.sh and added some new features

--- kali.sh
+++ kali.sh
@@ -1,13 +1,22 @@
-sudo tee -a /etc/apt/sources.list<<EOF
+sudo su
+
+tee -a /etc/apt/sources.list<<EOF
+deb http://http.kali.org/kali kali-last-snapshot main contrib non-free
+deb http://http.kali.org/kali kali-rolling main non-free contrib
+deb-src http://http.kali.org/kali kali-rolling main non-free contrib
 deb http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib
-deb http://http.kali.org/kali kali-rolling main contrib non-free
-deb-src http://http.kali.org/kali kali-rolling main contrib non-free
+deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main
+deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x bullseye main
+deb-src [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x bullseye main
+deb [arch=amd64,arm64,armhf] http://packages.microsoft.com/repos/code stable main
 EOF
 
-sudo apt-get update && sudo apt-get upgrade -y
+apt-get update && sudo apt-get upgrade -y
 
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
+
+sudo apt-get install gdebi
 
 git config --global user.name "tech-shamp"
 git config --global user.email shampytechnical@gmail.com
@@ -16,19 +25,30 @@ cd Downloads
 git clone https://github.com/vinceliuice/WhiteSur-kde.git
 cd WhiteSur-kde
 ./install.sh
+cd ..
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
 cd WhiteSur-icon-theme
 ./install.sh
+cd ..
 git clone https://github.com/vinceliuice/WhiteSur-cursors.git
 cd WhiteSur-cursors
 ./install.sh
-./build.sh
+cd ..
+
+sudo apt install snapd
+sudo systemctl unmask snapd.service
+sudo systemctl enable snapd.service
+sudo systemctl start snapd.service
+sudo snap install code --classic
+sudo snap install code-server --edge
 
 cd ~/Public
-git clone https://github.com/tech-shamp/nooreazal.git
 git clone https://github.com/tech-shamp/portfolio.git
-git clone https://github.com/tech-shamp/demo-example.git
 git clone https://github.com/tech-shamp/amathyst.git
-git clone https://github.com/tech-shamp/practice.git
+git clone https://github.com/tech-shamp/EducationTemplate
+code .
 
 cd ~
+
+cd Donwloads
+rm -rvf WhiteSur-kde WhiteSur-icon-theme WhiteSur-cursors
